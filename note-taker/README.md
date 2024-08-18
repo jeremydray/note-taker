a2497d98c56b8cc74d48da3b8b1d11301da482f3
[Jeremy Ray] <jeremydray@gmail.com>
2024-08-18 12:49:03 -0500

    Added delete functionality for note list and add README detail and images.

--- note-taker/README.md
+++ note-taker/README.md
@@ -1,1 +1,51 @@
-# note-taker
+  # Note Taker
+  ![License Badge](https://img.shields.io/badge/License-MIT-yellow.svg)
+  ## Table of Contents
+  1. [Instructions](#1-instructions)
+  2. [Description](#2-description)
+  3. [Contribution Guidelines](#3-contribution-guidelines)
+  4. [Testing the Application](#4-testing-the-application)
+  5. [License](#5-license)
+  6. [Questions](#6-questions)
+
+  ## 1. Instructions
+  The application requires Node.js with Express 4.16.4 and UUID 8.3.2 dependencies installed.
+
+  ## 2. Description
+  The purpose of the application is to allow the user to create, add to, and delete from a list of notes. When the user starts the application, they are taken to a splash screen where they can click "Get Started".
+  </br>
+  </br>
+  ![Splash Screen Example](./images/splash-screen.png)
+  
+  They are redirected to a page where they are presented with a template to enter a "Note Title" and "Note Text". 
+  </br>
+  </br>
+  ![Note Template Example](./images/note-template.png)
+  
+  As the user enters information into either field, they are provided with a "Clear Form" button to clear the form if desired. Once both fields are filled, the user is able to click a "Save Note" to clear out the form and add the note to the list on the side. The user can then click the sidebar notes to revisit and view the note text. 
+  </br>
+  </br>
+  ![Splash Screen Example](./images/submit-clear-options.png)
+  </br>
+  </br>
+  ![Splash Screen Example](./images/revisited-notes.png)
+
+  There is also a delete button that can be used to remove a note from the list.
+  </br>
+  </br>
+  ![Splash Screen Example](./images/deleted-note.png)
+
+  ## 3. Contribution Guidelines
+  The application uses pre-existing code, so only approved contributions can be added.
+
+  ## 4. Testing the Application
+  The application can be tested using the browser application or Insomnia for routing get/post/delete requests. 
+
+  ## 5. License
+  MIT</br>
+  https://opensource.org/licenses/MIT
+
+  ## 6. Questions:
+  Contact me with questions at:</br>
+    - Github: *jeremydray*</br>
+    - Email: *jeremydray@gmail.com*
--- note-taker/server.js
+++ note-taker/server.js
@@ -1,3 +1,4 @@
+//Imports Setup
 const { readFromFile,
     writeToFile,
     readAndAppend } = require('./helpers/utils')
@@ -8,38 +9,62 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//Middleware
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));// refer to 'public' folder first
 
-app.use(express.static('public'));
 
+//App Specifc Routes
+
+//List the existing or modified list of notes
 app.get('/notes', (req, res) =>
-    notesFile = res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//List the existing or modified list of notes
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json')
         .then((data) => res.json(JSON.parse(data)));
 });
 
+//Create new notes ()
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
-    console.log(req.body);
+    // console.log(req.body);
 
     if (title && text) {
         const newNote = {
-            title,
-            text,
+            title: title,
+            text: text,
             id: uuidv4(),
         }
 
         readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully`);
-    };
+        res.json(`Success`);
+    } else {
+        res.json('Error, notes need both Title and Text.')
+    }
 })
 
+//Delete notes from list when button is pressed
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    // console.log(noteId)
+    readFromFile('./db/db.json')
+        .then((details) =>
+            JSON.parse(details))
+        .then((json) => {
+            const result = json.filter((note) => note.id !== noteId);
+            // console.log(result)
+            writeToFile('./db/db.json', result);
+            res.json(`Item ${noteId} has been deleted 🗑️`);
+        });
+});
+
+//Internal testing port
 app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
 
 
